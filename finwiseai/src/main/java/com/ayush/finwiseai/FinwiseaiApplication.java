5779186794d8c27eaa82f936a831a7182f69dfa2
ayush <you@example.com>
2026-07-02 03:54:15 +0530

    Temporary debug endpoint to check env variables

--- finwiseai/src/main/java/com/ayush/finwiseai/FinwiseaiApplication.java
+++ finwiseai/src/main/java/com/ayush/finwiseai/FinwiseaiApplication.java
@@ -2,12 +2,12 @@ package com.ayush.finwiseai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class FinwiseaiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FinwiseaiApplication.class, args);
 	}
-
 }
